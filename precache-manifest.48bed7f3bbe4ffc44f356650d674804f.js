ace8f35b85a45b76df417001830492f0a908f188
hayden cordeiro <cordeirohayden@gmail.com>
2020-05-22 01:05:27 -0700

    Updates

---
+++ precache-manifest.48bed7f3bbe4ffc44f356650d674804f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1091716dfffc58f08a219f463e68f5c3",
+    "url": "/Hey-Chat/index.html"
+  },
+  {
+    "revision": "aa0b149e22413d26caa7",
+    "url": "/Hey-Chat/static/css/main.ef318c3c.chunk.css"
+  },
+  {
+    "revision": "9b09c3dce69f420083c8",
+    "url": "/Hey-Chat/static/js/2.5de6aaaa.chunk.js"
+  },
+  {
+    "revision": "fbfb5e42a41e7004782949e93fdf296a",
+    "url": "/Hey-Chat/static/js/2.5de6aaaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0b149e22413d26caa7",
+    "url": "/Hey-Chat/static/js/main.a8ade9f7.chunk.js"
+  },
+  {
+    "revision": "9b133022bd48bb1e89d7",
+    "url": "/Hey-Chat/static/js/runtime-main.e2882c31.js"
+  }
+]);
